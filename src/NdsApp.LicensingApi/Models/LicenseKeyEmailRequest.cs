cd07be2f591c65d1dbfa4f3da7b70e74f668c3a1
bimnodes <bimnodes@gmail.com>
2026-08-23 22:39:02 +0200

    Add requested license email model

---
+++ src/NdsApp.LicensingApi/Models/LicenseKeyEmailRequest.cs
@@ -0,0 +1,16 @@
+namespace NdsApp.LicensingApi.Models;
+
+public sealed class LicenseKeyEmailRequest
+{
+    public string ToEmail { get; init; } = string.Empty;
+
+    public string LicenseKey { get; init; } = string.Empty;
+
+    public string? PlanCode { get; init; }
+
+    public string? PlanName { get; init; }
+
+    public DateTimeOffset? ValidUntil { get; init; }
+
+    public int? MaxDevices { get; init; }
+}
